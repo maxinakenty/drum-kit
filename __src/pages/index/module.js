6810313d02c2ef21c6ae56b854ddf6820c947199
maxinakenty <mgreen097@gmail.com>
2017-01-16 15:55:58 +0300

    fixed js

--- __src/pages/index/module.js
+++ __src/pages/index/module.js
@@ -1,24 +1,22 @@
 function module() {
+	let keys = document.querySelectorAll('.key');
+	
 	function playSound(e) {
-		let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 		let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+		let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-		if (!audio) return;
-		audio.play();
+		if (!key) return;
 		key.classList.add('playing');
+		audio.play();
 	}
 
-	function removeTransition(e) {
-		console.log(e);
+	function removeTransform(e) {
 		if (e.propertyName !== 'transform') return;
 		this.classList.remove('playing');
-
 	}
-	
-	window.addEventListener('keydown', playSound);
-	let keys = document.querySelectorAll('.key');
 
-	keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+	window.addEventListener('keydown', playSound);
+	keys.forEach(key => key.addEventListener('transitionend', removeTransform));
 }
 
 export default module();
