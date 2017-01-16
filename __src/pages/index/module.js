c63b4caff12bb2c4c611ba6b87095fd0e7916788
maxinakenty <mgreen097@gmail.com>
2017-01-16 15:38:47 +0300

    added scripts

--- __src/pages/index/module.js
+++ __src/pages/index/module.js
@@ -1,5 +1,24 @@
 function module() {
-	console.log('Watch??');
+	function playSound(e) {
+		let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+		let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+		if (!audio) return;
+		audio.play();
+		key.classList.add('playing');
+	}
+
+	function removeTransition(e) {
+		console.log(e);
+		if (e.propertyName !== 'transform') return;
+		this.classList.remove('playing');
+
+	}
+	
+	window.addEventListener('keydown', playSound);
+	let keys = document.querySelectorAll('.key');
+
+	keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 }
 
 export default module();
